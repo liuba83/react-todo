6d46faeeb22782a084a599f210c40139cb9cc160
Liuba Barusch <liubabarusch@gmail.com>
2024-06-22 17:11:23 -0500

    feat: create AddTodoForm component

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,13 @@
+function AddTodoForm() {
+    return(
+        <div>
+            <form>
+                <label htmlFor="todoTitle">Title </label>
+                <input type="text" id="todoTitle" />
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTodoForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import TodoList from "./TodoList.jsx";
+import AddTodoForm from "./AddTodoForm.jsx";
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
