29851a3bd4cc6b4ef23ac33ef2fedcbd185e9954
Liuba Barusch <liubabarusch@gmail.com>
2024-07-01 01:02:41 -0500

    handle submit in AddTodoForm

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,13 +1,21 @@
 function AddTodoForm() {
-    return(
-        <div>
-            <form>
-                <label htmlFor="todoTitle">Title </label>
-                <input type="text" id="todoTitle" />
-                <button type="submit">Add</button>
-            </form>
-        </div>
-    )
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    let todoTitle = event.target.elements.title.value;
+    console.log(todoTitle);
+    event.target.reset();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <label htmlFor="todoTitle">Title </label>
+        <input name="title" type="text" id="todoTitle" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddTodoForm;
