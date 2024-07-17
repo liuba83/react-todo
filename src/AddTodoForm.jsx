f29e0a6190294b96da3e0def18778ea6a6c6461c
Liuba Barusch <liubabarusch@gmail.com>
2024-07-16 23:10:27 -0500

    destructuring props

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 
-function AddTodoForm(props) {
-  const [todoTitle, setTodoTitle] = React.useState('');
+function AddTodoForm({onAddTodo}) {
+  const [todoTitle, setTodoTitle] = React.useState("");
 
   function handleTitleChange(event) {
     let newTodoTitle = event.target.value;
     setTodoTitle(newTodoTitle);
   }
-  
+
   function handleAddTodo(event) {
     event.preventDefault();
     console.log(todoTitle);
-    props.onAddTodo({
+    onAddTodo({
       title: todoTitle,
-      id: Date.now()
+      id: Date.now(),
     });
-    setTodoTitle('');
+    setTodoTitle("");
   }
 
   return (
     <div>
       <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Title </label>
-        <input name="title" type="text" id="todoTitle" value={todoTitle} onChange={handleTitleChange} />
+        <input
+          name="title"
+          type="text"
+          id="todoTitle"
+          value={todoTitle}
+          onChange={handleTitleChange}
+        />
         <button type="submit">Add</button>
       </form>
     </div>
